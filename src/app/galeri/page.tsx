0cf5cfd8325b512b52576e11a63ddd24d4afc1d2
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2024-09-28 14:47:14 +0700

    feat: add page galeri

---
+++ src/app/galeri/page.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react';
+
+export default function Page(): JSX.Element {
+  return (
+    <main className="">
+      <div>
+        <h1>Galeri</h1>
+      </div>
+    </main>
+  );
+}
